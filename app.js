eed365741625e9f41c9d9894cc11502dd1f8e4dc
Rachel <rachelmmarriott@gmail.com>
2022-03-09 10:25:10 -0700

    update sql requests to join table data

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ db.connect(err => {
     if (err) throw err;
 });
 
-const promptUser = () => {
-    inquirer.prompt([
+const promptAction = () => {
+    console.log(`
+`)
+    return inquirer.prompt([
         {
             type: 'list',
             name: 'options',
@@ -27,26 +29,19 @@ const promptUser = () => {
     ])
         .then(({ options }) => {
             if (options === 'View All Departments') {
-                // THEN I am presented with a formatted table showing department names and department ids
                 displayDepartments();
             } else if (options === 'View All Roles') {
-                // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
                displayRoles();
             } else if (options === 'View All Employees') {
-                // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
                 displayEmployees();
             } else if (options === 'Add a Department') {
-                // THEN I am prompted to enter the name of the department and that department is added to the database
-                promptDepartment();
+                addDepartment();
             } else if (options === 'Add a Role') {
-                // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-                promptRole();
+                addRole();
             } else if (options === 'Add an Employee') {
-                // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-                promptEmployee();
+                addEmployee();
             } else if (options === 'Update an Employee Role') {
-                // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-                promptUpdateRole();
+                updateRole();
             }
         });
 }
@@ -58,38 +53,43 @@ const displayDepartments = () => {
     db.query(sql, (err, result) => {
         if(err) throw err;
         // TO DO: get formatted table to show instead of console log
+        console.log(result);
         console.table(result);
-        promptUser();
+        promptAction();
         })
 };
 
 // View All Roles
 const displayRoles = () => {
     // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-    const sql = `SELECT * FROM role`;
+    const sql = `SELECT role.title, department.name AS department_name 
+                FROM role
+                LEFT JOIN department ON role.department_id = department.id`;
     db.query(sql, (err, result) => {
         if(err) throw err;
         // TO DO: get formatted table to show instead of console log
         console.table(result);
-        promptUser();
+        promptAction();
         })
 };
 
 // View All Employees
 const displayEmployees = () => {
     // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-    const sql = `SELECT * FROM employee`;
+    const sql = `SELECT employee.first_name, employee.last_name, role.title AS role_title
+                FROM employee
+                LEFT JOIN role ON employee.role_id = role.id`;
     db.query(sql, (err, result) => {
         if(err) throw err;
         // TO DO: get formatted table to show instead of console log
         console.table(result);
-        promptUser();
+        promptAction();
         })
 };
 
 // If User chooses to 'Add a Department'
 // THEN I am prompted to enter the name of the department and that department is added to the database
-const promptDepartment = () => {
+const addDepartment = () => {
     return inquirer.prompt([
         {
             type: 'input',
@@ -117,13 +117,14 @@ const promptDepartment = () => {
             console.log(department + " has been added to the database.");
         })
     })
-    // TO DO: format this better so prompt user runs after the console log
-    .then(promptUser);
+    .then(promptAction);
+    // TO DO: FIX this so prompt user runs after the console log
+    // promptAction();
 };
 
 // If User chooses to 'Add a Role'
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-const promptRole = () => {
+const addRole = () => {
     inquirer.prompt([
         {
             type: 'input',
@@ -170,7 +171,7 @@ const promptRole = () => {
         const sql = `INSERT INTO role (title, salary, department)
                     VALUES (?, ?, ?)`;
         // TO DO: figure out how to link department with department_id (join tables first?)
-        const department_id = /
+        // const department_id = /
         const params = [title, salary, department_id];
         db.query(sql, params, (err, result) => {
             if (err) {
@@ -181,16 +182,35 @@ const promptRole = () => {
         })
     })
     // TO DO: format this better so prompt user runs after the console log
-    .then(promptUser);
+    .then(promptAction);
 };
 
 // If User chooses to 'Add an Employee'
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager
-const promptEmployee = () => {
+const addEmployee = () => {
+   
+    db.query(`SELECT * FROM role`, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        // let roleChoices = result;
+        console.log(result);
+    })
+
+    db.query(`SELECT * FROM employee WHERE role_id = 1`, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        // let managerChoices = result;
+        console.log(result);
+    })
+    
+    
+
     inquirer.prompt([
         {
             type: 'input',
-            name: 'firstName',
+            name: 'first_name',
             message: "What is the employee's first name?",
             validate: firstNameInput => {
                 if (firstNameInput) {
@@ -203,7 +223,7 @@ const promptEmployee = () => {
         },
         {
             type: 'input',
-            name: 'lastName',
+            name: 'last_name',
             message: "What is the employee's last name?",
             validate: lastNameInput => {
                 if (lastNameInput) {
@@ -215,40 +235,39 @@ const promptEmployee = () => {
             } 
         },
         {
-            type: 'input',
-            name: 'role',
+            type: 'expand',
+            name: 'role_id',
             message: "What is the employee's role?",
-            validate: roleInput => {
-                if (roleInput) {
-                    return true;
-                } else {
-                    console.log("Required");
-                    return false;
-                }
-            } 
+            choices: roleChoices,
         },
         {
-            type: 'input',
-            name: 'manager',
+            type: 'expand',
+            name: 'manager_id',
             message: "Who is the employee's manager?",
-            validate: managerInput => {
-                if (managerInput) {
-                    return true;
-                } else {
-                    console.log("Required");
-                    return false;
-                }
-            } 
+            choices: managerChoices
         },
     ])
-    .then((employee) => {
-        // THEN that employee is added to the database
+    .then(({ first_name, last_name, role_id, manager_id }) => {
+        // add employee to the database
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                    VALUES (?, ?, ?, ?)`;
+        const params = [first_name, last_name, role_id, manager_id];
+
+        db.query(sql, params, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(result);
+            console.log(first_name + " " + last_name + " has been added to the database.");
+        })
     })
+    // TO DO: format this better so prompt user runs after the console log
+    .then(promptAction);
 }
 
 // If User chooses to 'Update an Employee Role'
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-const promptUpdateRole = () => {
+const updateRole = () => {
     inquirer.prompt([
         {
             type: 'input',
@@ -288,10 +307,10 @@ const promptUpdateRole = () => {
         //     if(err) throw err;
         //     // TO DO: get formatted table to show instead of console log
         //     console.table(result);
-        //     promptUser();
+        //     promptAction();
         //     })
     })
 };
 
 // Start app with initial prompt
-promptUser()
+promptAction()
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,23 +2,34 @@ INSERT INTO department (name)
 VALUES
   ('Accounting'),
   ('Human Resources'),
-  ('Customer Service');
+  ('Customer Service'),
+  ('Marketing'),
+  ('IT'),
+  ('Sales');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ('Manager', '100000', '1'),
-  ('Intern', '30000', '2'),
-  ('Rep', '50000', '3');
+  ('Manager', '100000', 1),
+  ('Manager', '100000', 2),
+  ('Manager', '100000', 3),
+  ('Manager', '100000', 4),
+  ('Associate', '50000', 1),
+  ('Associate', '50000', 2),
+  ('Associate', '50000', 3),
+  ('Associate', '50000', 4),
+  ('Associate', '50000', 5),
+  ('Intern', '30000', 5),
+  ('Intern', '30000', 1);
   
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-  ('Ronald', 'Firbank', 1, 1),
-  ('Virginia', 'Woolf', 1, 1),
-  ('Piers', 'Gaveston', 1, 0),
+  ('Ronald', 'Firbank', 1, NULL),
+  ('Virginia', 'Woolf', 2, 1),
+  ('Piers', 'Gaveston', 2, 1),
   ('Charles', 'LeRoi', 2, 1),
   ('Katherine', 'Mansfield', 2, 1),
-  ('Dora', 'Carrington', 3, 0),
-  ('Edward', 'Bellamy', 3, 0),
-  ('Montague', 'Summers', 3, 1),
+  ('Dora', 'Carrington', 2, 1),
+  ('Edward', 'Bellamy', 2, 1),
+  ('Montague', 'Summers', 2, 1),
   ('Octavia', 'Butler', 3, 1),
-  ('Unica', 'Zurn', 1, 1);
+  ('Unica', 'Zurn', 3, 1);
